a4e6b29ee31389a99dcd6b6dbf3fe06fc937ae16
Ubuntu <ubuntu@ip-172-31-22-213.ca-central-1.compute.internal>
2021-01-28 18:17:00 +0000

    Some things fleshed out

--- api/test.js
+++ api/test.js
@@ -1,21 +1,79 @@
-function describeEndpoint( httpMethod, pathPattern, endpointBehaviorDescription ) {
-	this.httpMethod = httpMethod;
-	this.pathPattern = pathPattern;
+const request = require( 'supertest' );
 
-	describe( `${ httpMethod } ${ pathPattern}`, endpointBehaviorDescription );
+function describeEndpoint( httpMethod, pathPattern, endpointBehaviorDescription ) {
+	describe( `${ httpMethod } ${ pathPattern}`, function() {
+		before( function() {
+			this.httpMethod = httpMethod;
+			this.pathPattern = pathPattern;
+		} );
+
+		endpointBehaviorDescription();
+	} );
 }
 
-const itShouldRequireLoggedInAdmin = () => {};
+function itShouldRequireLoggedInAdmin() {
+	itShouldRequireLoggedInUser();
+
+	context( 'When the logged in user is not an admin', function() {
+		itShouldRespondWith( 403 );
+	} );
+}
+
 const itShouldValidate = () => {};
-const itShouldSaveRecord = () => {};
-const itShouldRespondWith = () => {};
-const itShouldRequireLoggedInUser = () => {};
+
+function itShouldSaveRecord( recordType, expectedRecord ) {
+	describe( 'Saved record', function() {
+		for ( const [ fieldName, expectedValue ] of Object.entries( expectedRecord ) ) {
+			it( `should include ${ fieldName }`, function() {
+				if ( 'function' === typeof expectedValue ) {
+					expect( expectedValue.call( this, this.savedRecord[ fieldName ] ) ).to.be.true;
+				} else {
+					expect( this.savedRecord[ fieldName ] ).to.equal( expectedValue );
+				}
+			} );
+		}
+	} );
+}
+
+function itShouldRespondWith( statusCode, expectedResponse ) {
+	it( `should respond with ${ statusCode }`, function() {
+		expect( this.res.status ).to.equal( statusCode );
+	} );
+
+	if ( expectedResponse ) {
+		describe( 'Response data', function() {
+			for ( const [ fieldName, expectedValue ] of Object.entries( expectedResponse ) ) {
+				it( `should include ${ fieldName }`, function() {
+					if ( 'function' === typeof expectedValue ) {
+						expect( expectedValue.call( this, this.savedRecord[ fieldName ] ) ).to.be.true;
+					} else {
+						expect( this.res.body[ fieldName ] ).to.equal( expectedValue );
+					}
+				} );
+			}
+		} );
+	}
+}
+
+function itShouldRequireLoggedInUser() {
+	context( 'When the user is not logged in', function() {
+		itShouldRespondWith( 401 );
+	} );
+}
+
 const itShouldContainRecordsWith = () => {};
 const itShouldRequireExistingRecord = () => {};
 const itShouldUpdateExistingRecord = () => {};
 const itShouldDeleteExistingRecord = () => {};
 
 function itShouldExcludeDatabaseFields( namesOfFieldsToConfirmAreNotPresent ) {
+}
+
+function performRequest( requestBody ) {
+	return request( this.app )
+	[ this.httpMethod.toLowerCase() ]( this.pathPattern )
+	.send( requestBody )
+	.then( res => this.res = res );
 }
 
 module.exports = {
@@ -30,4 +88,7 @@ module.exports = {
         itShouldUpdateExistingRecord,
         itShouldDeleteExistingRecord,
 	itShouldExcludeDatabaseFields,
+	requestMethods: {
+		performRequest,
+	},
 };
