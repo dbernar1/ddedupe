eb1be8b786500eb4741e6118eaf8b277366feb0d
Ubuntu <ubuntu@ip-172-31-22-213.ca-central-1.compute.internal>
2021-02-01 19:08:07 +0000

    Context provider for remote records

---
+++ react/remoteRecordProvider.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import { createContext, useContext, useState, } from 'react';
+import axios from 'axios';
+
+const createProviderCreator = ( {
+	useAuthHeaders, baseUrl,
+} ) => ( recordType, extras={}, ) => {
+	const Context = createContext();
+
+	const Provider = ( { children, } ) => {
+		const [ records, setRecords, ] = useState( null );
+		const capitalizedRecordType = recordType[ 0 ].toUpperCase() + recordType.substring( 1 );
+		const authHeaders = useAuthHeaders();
+
+		const value = {
+			records,
+			setRecords,
+			axios,
+			authHeaders,
+			getUrl( id ) {
+				return id
+					? `${ baseUrl }/${ recordType }/${ id }`
+					: `${ baseUrl }/${ recordType }`;
+			},
+			[ `${ recordType }s` ]: records,
+			[ `fetch${ capitalizedRecordType }s` ]: function() {
+				return axios.get( 
+					value.getUrl(),
+					{ headers: authHeaders, }
+				)
+				.then( ( { data, } ) => setRecords( data ) );
+			},
+			[ `delete${ capitalizedRecordType }` ]: function( id ) {
+				return axios.delete(
+					value.getUrl( id ),
+					{ headers: authHeaders, }
+				)
+				.then( () => {
+					if ( records ) {
+						setRecords(
+							records
+							.filter( record => id !== record.id )
+						);
+					}
+				} );
+			},
+			[ `create${ capitalizedRecordType }` ]: function( record ) {
+				return axios.post(
+					value.getUrl(),
+					record,
+					{ headers: authHeaders, }
+				)
+				.then( ( { data, } ) => {
+					if ( records ) {
+						setRecords(
+							[ data ].concat( records )
+						);
+					}
+				} );
+			},
+			[ `update${ capitalizedRecordType }` ]: function( id, record ) {
+				return axios.put(
+					value.getUrl( id ),
+					record,
+					{ headers: authHeaders, }
+				)
+				.then( () => {
+					if ( records ) {
+						const recordLocation = records.findIndex( record => id === record.id );
+
+						const newRecords = records
+							.filter( record => id !== record.id );
+
+						newRecords.splice(
+							recordLocation,
+							0,
+							{
+								...record,
+								id,
+							}
+						);
+
+						setRecords( newRecords );
+					}
+				} );
+			},
+		};
+
+		for ( const extraName in extras ) {
+			value[ extraName ] = function() {
+				return extras[ extraName ].apply( value, arguments );
+			};
+		}
+
+		return React.createElement( Context.Provider, { value }, children );
+	};
+
+	return [
+		() => useContext( Context ),
+		Provider,
+	];
+};
+
+export default createProviderCreator;
